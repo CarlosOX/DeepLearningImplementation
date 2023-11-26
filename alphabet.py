36765a326c31df449e6c80094416f847e11014a1
CarlosOX <84168967+CarlosOX@users.noreply.github.com>
2023-11-26 16:01:14 -0600

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -8,6 +8,8 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 import random
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
 
 train_dir = 'datos/asl_alphabet_train/asl_alphabet_train'
 
@@ -47,14 +49,12 @@ test_generator = test_datagen.flow_from_directory(
 
 
 
-# Build the Sequential convolutional neural network model
-
+# Construccion del modelo 
 def get_model(input_shape):
  
     model  =  Sequential([
                     Conv2D(32, kernel_size=3, padding='same', activation="relu", input_shape=input_shape),
                     MaxPooling2D((2,2)),
-                    #Flatten(),
                     Conv2D(32, kernel_size=3, padding='same', strides = 1, activation="relu"),
                     Conv2D(16, kernel_size=3, padding='same', strides = 1, activation="relu"),
                     Conv2D(16, kernel_size=3, padding='same', strides = 1, activation="relu"),
@@ -82,10 +82,10 @@ def train_model(model,train_generator,val_generator):
         # Entrenar el modelo usando el generador de datos
     history = model.fit(
         train_generator,
-        steps_per_epoch =200,
+        steps_per_epoch =50,
         epochs=20,  # Número de épocas de entrenamiento
         validation_data=val_generator,
-        validation_steps = 100
+        validation_steps = 50
     )
 
     return history
@@ -117,22 +117,55 @@ while True:
         accuracy = history.history['accuracy']
         val_accuracy = history.history['val_accuracy']
 
-        plt.figure(figsize=(12, 4))
+        plt.figure(figsize=(15, 5))
+
+        # Gráfico de pérdida
         plt.subplot(1, 2, 1)
         plt.plot(loss, label='Training Loss')
         plt.plot(val_loss, label='Validation Loss')
         plt.legend()
         plt.title('Loss vs. Epochs')
 
+        # Gráfico de precisión
         plt.subplot(1, 2, 2)
         plt.plot(accuracy, label='Training Accuracy')
         plt.plot(val_accuracy, label='Validation Accuracy')
         plt.legend()
         plt.title('Accuracy vs. Epochs')
-
-        # Mostrar todos los graficos al mismo tiempo
+        # Ajustar diseño
+        plt.tight_layout()
         plt.show()
 
+
+
+        opcion_matriz = input("¿Desea ver la matriz de confusión? (y/n): ")
+
+        if opcion_matriz.lower() == "y":
+            plt.figure(figsize=(8, 6))
+            # Matriz de confusión
+            y_true = []
+            y_pred = []
+
+            for i in range(len(test_generator)):
+                test_samples, test_labels = test_generator[i]
+                predictions = model.predict(test_samples)
+                y_true.extend(np.argmax(test_labels, axis=1))
+                y_pred.extend(np.argmax(predictions, axis=1))
+
+            cm = confusion_matrix(y_true, y_pred)
+            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                        xticklabels=test_generator.class_indices.keys(),
+                        yticklabels=test_generator.class_indices.keys(),
+                        cbar=False)
+            plt.title('Confusion Matrix')
+            plt.show()
+
+          
+        # Informe de clasificación
+        target_names = list(test_generator.class_indices.keys())
+        print(classification_report(y_true, y_pred, target_names=target_names))
+
+    
     elif opcion == "2":
 
         # Elegir un índice de muestra aleatorio
